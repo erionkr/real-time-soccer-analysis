1c29ea4883302f82f3a69d092837b2352d33c0cf
Erion Krasniqi <erionkrasniqi@MacBook-Air-von-Erion.local>
2025-05-10 17:48:45 +0200

    added a spark testing script, to test our configured environment

---
+++ scripts/test_spark.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("KafkaTest") \
+    .master("spark://localhost:7077") \
+    .getOrCreate()
+
+df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "test-topic") \
+    .load()
+
+df.selectExpr("CAST(value AS STRING)").writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .start() \
+    .awaitTermination()
